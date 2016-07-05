5101c8b4bbfe1c4e8f2eacb1aa628bf64d06e4be
sasha <zavgoroa@yahoo.com>
2016-07-05 16:38:36 +0300

    Вывод ошибок.

--- src/gobotnet/file_operation.go
+++ src/gobotnet/file_operation.go
@@ -11,17 +11,22 @@ import (
 
 func CheckFileExist(filePath string) error {
 	if _, err := os.Stat(filePath); os.IsNotExist(err) {
+		OutMessage(err.Error())
 		return err
 	}
 	return nil
 }
 
-func CreateDir(dirPath string) error {
-	return os.MkdirAll(dirPath, 0777)
+func CreateDir(dirPath string, fileMode os.FileMode) error {
+	err := os.MkdirAll(dirPath, fileMode)
+	OutMessage(err.Error())
+	return err
 }
 
 func ReadFile(nameFile string) (bytesFile []byte, err error) {
-	return ioutil.ReadFile(nameFile)
+	readBytes, err := ioutil.ReadFile(nameFile)
+	OutMessage(err.Error())
+	return readBytes, err
 }
 
 func SaveImageToFile(image *image.RGBA, nameFile string) error {
@@ -30,59 +35,67 @@ func SaveImageToFile(image *image.RGBA, nameFile string) error {
 		OutMessage(err.Error())
 		return err
 	}
+
 	err = png.Encode(f, image)
 	if err != nil {
 		OutMessage(err.Error())
 		return err
 	}
 	f.Close()
+
 	return nil
 }
 
 func CopyFileToDirectory(pathSourceFile string, pathDestFile string) error {
 	sourceFile, err := os.Open(pathSourceFile)
 	if err != nil {
+		OutMessage(err.Error())
 		return err
 	}
 	defer sourceFile.Close()
 
 	destFile, err := os.Create(pathDestFile)
 	if err != nil {
+		OutMessage(err.Error())
 		return err
 	}
 	defer destFile.Close()
 
 	_, err = io.Copy(destFile, sourceFile)
 	if err != nil {
+		OutMessage(err.Error())
 		return err
 	}
 
 	err = destFile.Sync()
 	if err != nil {
+		OutMessage(err.Error())
 		return err
 	}
 
 	sourceFileInfo, err := sourceFile.Stat()
 	if err != nil {
+		OutMessage(err.Error())
 		return err
 	}
 
 	destFileInfo, err := destFile.Stat()
 	if err != nil {
+		OutMessage(err.Error())
 		return err
 	}
 
 	if sourceFileInfo.Size() == destFileInfo.Size() {
 	} else {
-		return errors.New("Bad copy file")
+		err = errors.New("Bad copy file")
+		OutMessage(err.Error())
+		return err
 	}
 	return nil
 }
 
 func DeleteFile(nameFile string) error {
 	err := os.Remove(nameFile)
-	if err != nil {
-		OutMessage(err.Error())
-	}
+	OutMessage(err.Error())
 	return err
 }
